c035f4c7c5ce021f54924897b705a1c1a9934de1
denis-mahei <denmahei@gmail.com>
2025-07-07 21:36:25 +0300

    add validation to contactSchema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,13 +1,25 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required!',
+  }),
   phoneNumber: Joi.string()
     .pattern(/^\+?[0-9]{10,15}$/)
     .required(),
   email: Joi.string().email({ minDomainSegments: 2 }),
   isFavorite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({
+      'string.base': 'Contact type most be string',
+      'any.only': 'Contact type must be one of: work, home, or personal',
+      'any.required': 'Contact type is required!',
+    }),
 });
 
 export const updateContactSchema = Joi.object({
